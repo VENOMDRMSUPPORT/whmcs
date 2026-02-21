0fc82f1b550505dc1bd0036041e714cdd29d6bfe
VENOMDRMSUPPORT <support@venom-drm.com>
2026-02-21 13:32:31 +0200

    Initial commit

---
+++ templates_c/88fc28b4e13b2899a005652b24896ce317022de8_0.string.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.48, created on 2026-02-21 11:13:40
+  from '88fc28b4e13b2899a005652b24896ce317022de8' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_699993646e0880_43177314',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_699993646e0880_43177314 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="wizard-transition-step">
+    <div class="icon"><i class="far fa-lightbulb"></i></div>
+    <div class="title"><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>"wizard.welcome"),$_smarty_tpl ) );?>
+</div>
+    <div class="tag"><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>"wizard.intro"),$_smarty_tpl ) );?>
+</div>
+    <div class="greyout"><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>"wizard.noTime"),$_smarty_tpl ) );?>
+</div>
+</div><?php }
+}
---
+++ templates_c/c367de657c8e786ecd9c1d85e1b6ef9d14254994_0.file.network-issues-notifications.tpl.php
@@ -0,0 +1,54 @@
+<?php
+/* Smarty version 3.1.48, created on 2026-02-21 11:29:02
+  from '/home/venom/workspace/venom-drm.test/templates/twenty-one/includes/network-issues-notifications.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.48',
+  'unifunc' => 'content_699996fe3842a2_88073598',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c367de657c8e786ecd9c1d85e1b6ef9d14254994' => 
+    array (
+      0 => '/home/venom/workspace/venom-drm.test/templates/twenty-one/includes/network-issues-notifications.tpl',
+      1 => 1771668545,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_699996fe3842a2_88073598 (Smarty_Internal_Template $_smarty_tpl) {
+if ($_smarty_tpl->tpl_vars['openNetworkIssueCounts']->value['open'] > 0) {?>
+    <div class="alert alert-warning network-issue-alert m-0">
+        <div class="container">
+            <i class="fas fa-exclamation-triangle fa-fw"></i>
+            <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>'networkIssuesAware'),$_smarty_tpl ) );?>
+
+            <a href="<?php echo $_smarty_tpl->tpl_vars['WEB_ROOT']->value;?>
+/serverstatus.php" class="alert-link float-lg-right">
+                <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>'learnmore'),$_smarty_tpl ) );?>
+
+                <i class="far fa-arrow-right"></i>
+            </a>
+        </div>
+    </div>
+<?php } elseif ($_smarty_tpl->tpl_vars['openNetworkIssueCounts']->value['scheduled'] > 0) {?>
+    <div class="alert alert-info network-issue-alert m-0">
+        <div class="container">
+            <i class="fas fa-info-circle fa-fw"></i>
+            <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>'networkIssuesScheduled'),$_smarty_tpl ) );?>
+
+            <a href="<?php echo $_smarty_tpl->tpl_vars['WEB_ROOT']->value;?>
+/serverstatus.php" class="alert-link float-lg-right">
+                <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['lang'][0], array( array('key'=>'learnmore'),$_smarty_tpl ) );?>
+
+                <i class="far fa-arrow-right"></i>
+            </a>
+        </div>
+    </div>
+<?php }
+}
+}
